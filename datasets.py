14ad40a4c928dfdac408dae5dbf49b9f89b234da
Mino-Irk <e155734@gmail.com>
2017-10-24 15:50:54 +0900

    add dataset.py

---
+++ datasets.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*-coding: utf-8-*-
+
+import numpy as np
+
+def load_linear_example1():
+    X = np.array([[1,4],[1,8],[1,13],[1,17]])
+    Y = np.array([7,10,11,14])
+    return X,Y
